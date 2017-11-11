9e49a73c21b35eb3a74fe2fc6ca6344ce7f53175
chrism <ajackal244@gmail.com>
2017-11-10 22:26:15 -0500

    Create define_a_function.py

---
+++ Lesson 1/define_a_function.py
@@ -0,0 +1,8 @@
+# Line 5: defines a function called 'print_hello_world', that takes no arguments ()
+# Line 6: defines what the function will do (print a string)
+# Line 8: calls the function; without this the function is defined but never called
+
+def print_hello_world():
+	print "Hello World!"
+	
+print_hello_world()
